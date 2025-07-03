ed9e0d96e10dd6daa23e07e08a2224945ec1b77f
Dustin-Poff <32342126+Dustin-Poff@users.noreply.github.com>
2025-07-03 00:21:04 +0000

    Adding comments

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -2,11 +2,13 @@
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
 #include <iostream>
-
+/*This program is creating and displaying the message "Hello World!" in the terminal.*/
 int main()
 {
+    //This line sends a set of characters "Hello World!" plus a new line out to the terminal.
     std::cout << "HELLO WORLD!" << std::endl;
-
+    //This line sends a two new lines out to the terminal. 
+    //Purposes are to seperate the function output from that of the end program output.
     std::cout << std::endl << std::endl;
     return 0;
 }
