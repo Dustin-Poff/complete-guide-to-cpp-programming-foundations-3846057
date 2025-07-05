87a2aa87da6c2fc0cd658ee4751f45403601c2d9
Dustin-Poff <32342126+Dustin-Poff@users.noreply.github.com>
2025-07-05 19:23:33 +0000

    making the changes that correspond with the example on chapter 2 leasson 5.

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -4,8 +4,32 @@
 
 #include <iostream>
 
+//creates two varaibles a and b then sets b to equal 5 as global varaibles.
+int a, b = 5;
+
 int main(){
+    using std::cout;
+    using std::endl;
     
-    std::cout << std::endl << std::endl;
+    bool my_flag;
+    unsigned int positive;
+    
+    a = 7;
+    my_flag = false;
+    
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "flag = " << my_flag << endl;
+
+    my_flag = true;
+
+    cout << "flag = " << my_flag << endl;
+    cout << "a + b = " << a + b << endl;
+    cout << "b - a = " << b - a << endl;
+
+    positive = b - a;
+    cout << "b - a (unsigned)  = " << positive << endl;
+
+    cout << endl << endl;
     return 0;
 }
